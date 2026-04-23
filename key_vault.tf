475ae83a342c2c081d711d18f4dc5cf49eb3fa81
Alexandre C. <188470145+AlexCAMBOU-EZT@users.noreply.github.com>
2026-04-23 10:57:25 +0200

    Bootstrap existing resources in workflow

--- key_vault.tf
+++ key_vault.tf
@@ -13,13 +13,13 @@ resource "random_string" "ssh_kv_suffix" {
 }
 
 resource "azurerm_key_vault" "ssh" {
-  name                        = substr("kv${local.key_vault_name_prefix}${random_string.ssh_kv_suffix.result}", 0, 24)
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+  name                       = substr("kv${local.key_vault_name_prefix}${random_string.ssh_kv_suffix.result}", 0, 24)
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
