2e0b4aa274299753cd9414f27b7e185b7d150c77
Alexandre C. <188470145+AlexCAMBOU-EZT@users.noreply.github.com>
2026-04-23 10:21:39 +0200

    Protect backend from destroy and archive errored state

--- state_storage.tf
+++ state_storage.tf
@@ -17,10 +17,18 @@ resource "azurerm_storage_account" "tfstate" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = var.tfstate_container_name
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
